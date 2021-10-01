7c4857943ab015896d2ec45941544d5814a8a14d
Rangewest <sasn026@gmail.com>
2021-10-01 19:28:13 +0300

    .

--- SL.js
+++ SL.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.ham').click(function(){
+        $('.ham span').toggleClass('active')
         $('.linkm').toggleClass('active')
         $('.linkm ul').toggleClass('active')
+        $('#close').toggleClass('active')
     })
 })
